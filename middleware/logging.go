5d71ad9b186bd2bacc195921e2cbc09478bf7cfb
Kwasi Gyasi-Agyei <kwasi@edged.agency>
2017-03-27 07:01:43 +0200

    Added more logging details for before and after request

--- middleware/logging.go
+++ middleware/logging.go
@@ -3,8 +3,9 @@ package middleware
 import (
 	"net/http"
 
+	"github.com/edgedagency/mulungu/logger"
+
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/log"
 )
 
 //Logging this handler determins and set appropriate namespace on re
@@ -12,9 +13,15 @@ func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := appengine.NewContext(r)
 
-		log.Debugf(ctx, "**MIDDLEWARE** logging request information")
+		logger.Debugf(ctx, "middleware logging", "logging request information")
 
-		log.Debugf(ctx, "URI:%s DefaultHostName:%s Host:%s Path:%s Referer:%s IP:%s Scheme:%s Method:%s X-Namespace:%s appId:%s datacenter:%s environment:%s",
+		logger.Debugf(ctx, "middleware logging", "request headers")
+		for header, value := range r.Header {
+			logger.Debugf(ctx, "middleware logging", "Header: %s Value: %s", header, value)
+		}
+
+		logger.Debugf(ctx, "middleware logging", "request")
+		logger.Debugf(ctx, "middleware logging", "BEFORE: URI:%s DefaultHostName:%s Host:%s Path:%s Referer:%s IP:%s Scheme:%s Method:%s X-Namespace:%s appId:%s datacenter:%s environment:%s proxy-host:%s",
 			r.URL.RequestURI(),
 			appengine.DefaultVersionHostname(ctx),
 			r.URL.Host,
@@ -26,9 +33,24 @@ func Logging(next http.Handler) http.Handler {
 			r.Header.Get("X-Namespace"),
 			appengine.AppID(ctx),
 			appengine.Datacenter(ctx),
-			r.Header.Get("X-Environment"))
+			r.Header.Get("X-Environment"),
+			r.Header.Get("X-PROXY-HOST"))
 
 		next.ServeHTTP(w, r)
 
+		logger.Debugf(ctx, "middleware logging", "AFTER: URI:%s DefaultHostName:%s Host:%s Path:%s Referer:%s IP:%s Scheme:%s Method:%s X-Namespace:%s appId:%s datacenter:%s environment:%s proxy-host:%s",
+			r.URL.RequestURI(),
+			appengine.DefaultVersionHostname(ctx),
+			r.URL.Host,
+			r.URL.Path,
+			r.Referer(),
+			r.RemoteAddr,
+			r.URL.Scheme,
+			r.Method,
+			r.Header.Get("X-Namespace"),
+			appengine.AppID(ctx),
+			appengine.Datacenter(ctx),
+			r.Header.Get("X-Environment"),
+			r.Header.Get("X-PROXY-HOST"))
 	})
 }
