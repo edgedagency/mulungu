9174cc8d24b6f08bf799ae22c112695e6435716f
Kwasi Gyasi-Agyei <kwasi@edged.agency>
2017-05-24 21:32:11 +0200

    Added steps to configurations file to share thinking

--- middleware/configuration.go
+++ middleware/configuration.go
@@ -3,58 +3,29 @@ package middleware
 import (
 	"net/http"
 
-	"golang.org/x/net/context"
-
-	"github.com/edgedagency/mulungu/configuration"
-	"github.com/edgedagency/mulungu/constant"
-
 	"google.golang.org/appengine"
+
+	"github.com/edgedagency/mulungu/constant"
+	"github.com/edgedagency/mulungu/logger"
 )
-
-//type key int
-
-const requestIDKey key = 0
-
-//Configuration this middleware will load configurations and pass on to rest of request
-func Configuration(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := appengine.NewContext(r.Context())
-
-		//READ THIS: https://www.nicolasmerouze.com/share-values-between-middlewares-context-golang/
-		configuration.NewConfigurationEntryModel(ctx, r.Header.Get(constant.HeaderNamespace))
-		//LETS CONSIDER SOMTHING LIKE this
-
-		// ctx.Value(key) <=== Where does it get it's value from based on key, how do we populate the ctx
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-//NewContextWithRequestID populates context so that for each request theres an ID associated with it
-func NewContextWithRequestID(ctx context.Context, r *http.Request, key string) context.Context {
-	reqID := r.Header.Get(key) //Gets "X-Request-ID from HTTP Header "
-
-	return context.WithValue(ctx, requestIDKey, reqID)
-
-}
 
 //ConfigurationMiddleware create a new Request value from context, and pass it onto the next handler .
 func ConfigurationMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := appengine.NewContext(r)
 
-		ctx := NewContextWithRequestID(r.Context(), r)
-		configuration.NewConfigurationEntryModel(ctx, r.Header.Get(constant.HeaderNamespace))
+		// 1. obtain namespace from header
+		namespace := r.Header.Get(constant.HeaderNamespace)
+		logger.Debugf(ctx, "middleware configurations", "getting configurations with namespace %s", namespace)
+
+		// 2. call model which retireves all configurations entries
+		// configurationsModel := configuration.NewConfigurationEntryModel(ctx, namespace)
+
+		// 3. execute findAll on model to return entries from datastore
+		// configurations := configurationsModel.findAll()
+		// logger.Debugf(ctx, "middleware configurations", "getting configurations with namespace %#v", configurations)
 
 		next.ServeHTTP(w, r)
+
 	})
-}
-
-//RequestIDFromContext populates its context with value key
-func RequestIDFromContext(ctx context.Context, key string) string {
-	return ctx.Value(key)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	reqID := RequestIDFromContext(r.Context())
-	return w, reqID
 }
