fee967bff79ba93d2f6c64c338da322d5cca7399
Kwasi Gyasi-Agyei <kwasi@edged.agency>
2017-07-18 16:43:00 +0200

    update contact to by type/value

--- core/contact.go
+++ core/contact.go
@@ -2,18 +2,17 @@ package core
 
 import (
 	"cloud.google.com/go/datastore"
-	"github.com/edgedagency/mulungu/util"
 )
 
-//Contact represents user contact details
-type Contact map[string]map[string]string
+//Contact represents a contact phone (000) 0000 email example@example.com endless contacts
+type Contact map[string]string
 
 //Load function from PropertyLoaderInterface helps datastore load this object
 func (c *Contact) Load(dp []datastore.Property) error {
 	*c = make(Contact)
 
 	for _, property := range dp {
-		(*c)[property.Name] = util.InterfaceToMapString(property.Value)
+		(*c)[property.Name] = property.Value.(string)
 	}
 
 	return nil
@@ -26,18 +25,16 @@ func (c *Contact) Save() ([]datastore.Property, error) {
 	for name, value := range *c {
 		propertise = append(propertise, datastore.Property{Name: name,
 			NoIndex: true,
-			Value:   util.MapToJSONString(value)})
+			Value:   value})
 	}
 
 	return propertise, nil
 }
 
-//Get returns contact map based on name
-func (c *Contact) Get(contactName string) map[string]string {
-	for name, value := range *c {
-		if contactName == name {
-			return value
-		}
+//Get returns contact based on key
+func (c *Contact) Get(key string) string {
+	if value, ok := (*c)[key]; ok {
+		return value
 	}
-	return nil
+	return ""
 }
