503fb444d964b12dd258bf46eee2541978be957b
Kwasi Gyasi-Agyei <kwasi@edged.agency>
2017-10-04 17:44:18 +0200

    Common service functions

---
+++ service.go
@@ -0,0 +1,80 @@
+package mulungu
+
+import (
+	"golang.org/x/net/context"
+
+	"github.com/edgedagency/mulungu/core"
+	"github.com/edgedagency/mulungu/logger"
+	"github.com/edgedagency/mulungu/util"
+)
+
+//Service represenetation of a service
+type Service struct {
+	context   context.Context
+	namespace string
+	kind      string
+}
+
+//Init initiates this services
+func (s *Service) Init(context context.Context, namespace, kind string) {
+	s.context = context
+	s.namespace = namespace
+	s.kind = kind
+}
+
+//Kind returns kind
+func (s *Service) Kind() string {
+	return s.kind
+}
+
+//Namespace returns namespace
+func (s *Service) Namespace() string {
+	return s.namespace
+}
+
+//Context returns context
+func (s *Service) Context() context.Context {
+	return s.context
+}
+
+//Find returns application based on id
+func (s *Service) Find(id string) (interface{}, error) {
+
+	//2. save record
+	datastoreModel := core.NewDatastoreModel(s.Context(), s.Namespace(), s.Kind(), nil, nil)
+	datastoreModel.Query = (&core.DatastoreQuery{}).AddFilter(&core.DatastoreFilter{Key: "__key__", Operation: "=", Value: id})
+	responseRecord, responseError := datastoreModel.Get(nil)
+
+	if responseError != nil {
+		return nil, responseError
+	}
+
+	//return first item
+	if len(responseRecord) > 0 {
+		return responseRecord[0], nil
+	}
+
+	return responseRecord, nil
+}
+
+//FindAll finds all application from datastore
+func (s *Service) FindAll(filter string) (interface{}, error) {
+	datastoreModel := core.NewDatastoreModel(s.Context(), s.Namespace(), s.Kind(), nil, nil)
+	datastoreModel.Query = core.NewDatastoreQuery().BuildQuery(s.Context(), filter)
+	responseRecord, responseError := datastoreModel.Get(nil)
+
+	if responseError != nil {
+		return nil, responseError
+	}
+
+	return responseRecord, nil
+}
+
+//Publish publish to topic
+func (s *Service) Publish(topic string, record map[string]interface{}, attributes map[string]string) {
+	publishID, publishErr := util.PubSubTopicPublish(s.Context(), util.PubSubTopicID(s.Namespace(), topic), record, attributes)
+	if publishErr != nil {
+		logger.Errorf(s.Context(), "application service", "failed to publish record created %s", publishErr.Error())
+	}
+	logger.Debugf(s.Context(), "application service", "published record created, publish id %s", publishID)
+}
